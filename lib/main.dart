4b4280b8df98c1df4b73a5de0d28af1432eaa77b
Md Abdullah <spark8ab@gmail.com>
2022-08-06 14:32:25 +0600

    28 lesson ends here

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("HI"),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("This is my default text!"),
+      ),
+    );
   }
-
 }
