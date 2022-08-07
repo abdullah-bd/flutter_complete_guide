2898a6d5c316c7f7bc766541dc4ab9baee0327aa
Md Abdullah <spark8ab@gmail.com>
2022-08-07 14:52:08 +0600

    40 lesson ends here Container and styling.

--- lib/question.dart
+++ lib/question.dart
@@ -2,9 +2,19 @@ import 'package:flutter/cupertino.dart';
 
 class Question extends StatelessWidget {
   final String questionText;
+
   Question(this.questionText);
+
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
